92556ea580f95c141f3108cd69d18abd006d06ca
Shwetabhdixit <shwetabh.dixit@gmail.com>
2020-10-20 01:33:35 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify, request
+import requests
+import ssl
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain('C:/Users/Dell/Downloads/cert.pem',
+                        'C:/Users/Dell/Downloads/key_a4dbf961-80de-48e5-ae02-2f5c187b67ab.pem')
+
+
 app = Flask(__name__)
 
 #need to figure out the response we will give for each of these API and modify them
 
-@app.route("/merchantFinder",methods= ['GET','POST'])
+@app.route("/")
 def index():
-    if(request.method == 'POST'):
-        some_json = request.get_json()
-        return jsonify({'you sent':some_json})
+    return "Login Page";
+
+@app.route("/merchantLocator",methods= ['GET','POST'])
+def index1():
+    if(request.method == 'GET'):
+
+        url = "https://sandbox.api.visa.com/merchantlocator/v1/locator"
+
+        payload = " \r\n{\r\n\"searchOptions\": {\r\n\"matchScore\": \"true\",\r\n\"maxRecords\": \"5\",\r\n\"matchIndicators\": \"true\"\r\n},\r\n\"header\": {\r\n\"startIndex\": \"0\",\r\n\"requestMessageId\": \"Request_001\",\r\n\"messageDateTime\": \"2020-10-19T10:39:49.903\"\r\n},\r\n\"searchAttrList\": {\r\n\"distanceUnit\": \"M\",\r\n\"distance\": \"2\",\r\n\"merchantCountryCode\": \"840\",\r\n\"latitude\": \"37.363922\",\r\n\"longitude\": \"-121.929163\",\r\n\"merchantName\": \"Starbucks\"\r\n},\r\n\"responseAttrList\": [\r\n\"GNLOCATOR\"\r\n]\r\n}"
+        headers = {
+            'Authorization': 'Basic RjE4NjBWMjA3WjlLS0dMNlhENVMyMWNaWmEyNWVwSnlXWTNHaEhXQ3N2dmEydUpIRTpqajJHWERORw==',
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text.encode('utf8'))
+        return response.text
+
     else:
         return jsonify({"error":"invalid input"})
 
 @app.route("/transactionScore",methods= ['GET','POST'])
-def index():
+def index2():
     if(request.method == 'POST'):
         some_json = request.get_json()
         return jsonify({'you sent':some_json})
@@ -21,6 +44,7 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   # context  = ('C:/Users/Dell/Downloads/key_a4dbf961-80de-48e5-ae02-2f5c187b67ab.pem','C:/Users/Dell/Downloads/cert.pem')
+    app.run(debug=True,ssl_context=context)
 
 
